a79d48d22b96b7f8b5552042113eb3d8f87b1aa7
Jake Goldsborough <rjgoldsborough@gmail.com>
2011-09-09 01:23:49 -0400

    cleaning up variables and subroutines

--- twitcli.pl
+++ twitcli.pl
@@ -24,6 +24,12 @@ my $nt = Net::Twitter->new(
     consumer_secret => $consumer_secret,
 );
 
+  my $term = new Term::ReadLine 'twitcli$';
+  my $status_command = 'status';
+  my $update_command = 'update';
+
+
+
 # check for file existence because otherwise the access tokens are not present - don't know if this is a best practice, need feedback
 if (-e "$home_dir/.twitcli") {
   my $twitter_config = YAML::XS::LoadFile( "$home_dir/.twitcli" );
@@ -47,24 +53,6 @@ unless ( $nt->authorized ) {
 
 show_friends_timeline();
 
-# subroutine to show the twitcli command line prompt
-sub twitcli_command_prompt {
-  my $term = new Term::ReadLine 'twitcli$';
-  my $username = $nt->user_timeline();
-  my $status_command = 'status';
-  my $update_command = 'update';
-  print "$username->[0]{user}{screen_name}\@twitcli\$ ";
-  my $command = <STDIN>;
-
-  if ( $command eq $update_command ) {
-    show_friends_timeline();
-  }  
-  
-  if ( $command eq $status_command ) {
-    update_status();
-  }
-}
-
 # subroutine to show your friends status updates in a timeline
 sub show_friends_timeline {
   my $statuses = $nt->friends_timeline({ count => 100 });
@@ -80,4 +68,19 @@ sub update_status {
     my $status_update = <STDIN>;
     $nt->update($status_update);
     twitcli_command_prompt();
+}
+
+# subroutine to show the twitcli command line prompt
+sub twitcli_command_prompt {
+  my $username = $nt->user_timeline();
+  print "$username->[0]{user}{screen_name}\@twitcli\$ ";
+  my $command = <STDIN>;
+
+  if ( $command == 'update' ) {
+    show_friends_timeline();
+  }  
+  
+  if ( $command == 'status' ) {
+    update_status();
+  }
 }
